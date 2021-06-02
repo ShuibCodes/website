3f42974b1e9909b78bf7bd8e3f7611dd2ee10c2e
shuibcodes <abdullahshuib124@gmail.com>
2021-06-02 16:36:30 +0100

    css

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import '../index.css'
+
+function Features() {
+    return (    
+      <>
+          
+
+        <div  class="grid-container">
+        <div class="the-feed">The Feed
+            <h1>A CRM that fits your business</h1>
+            <p>Freshworks CRM adapts to the way your team works.</p>
+      
+        </div>
+        <div className="featureLeft">
+     
+            <h2>Contact Lifecycle</h2>
+            <div className="features">
+            <p> A contact goes through various stages in their lifecycle. Create your own lifecycle stages based on your business process.,</p>
+            </div>
+          
+          
+        </div>
+   
+        
+        
+        
+        
+        <div className="featureRight" >
+     
+            <h2>Custom Fields</h2>
+            <div className="features">
+            <p> took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+            </div>
+            
+        </div>
+        <div className="marginBottom">
+        <div className="featureLeft">
+     
+            <h2>Custom Sales Activities </h2>
+            <div className="features">
+            <p> took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+            </div>
+            
+        </div>
+        </div>
+    
+         <div class="marginBottom">
+         <div className="featureRight">
+       
+             <h2>Multi Currency</h2>
+                <div className="features">
+                <p> took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+                </div>
+        </div>
+
+         </div>
+      </div>
+     
+  
+    
+    </>
+    )
+}
+
+export default Features
